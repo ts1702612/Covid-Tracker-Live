8a18befa411940dfaee4b62bd036a8408e99e0aa
taruna <tarunaagrawal2017@gmail.com>
2020-07-23 19:29:53 +0530

    Updates

---
+++ precache-manifest.292780eb0db233ef5bc2ec1472de9e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ebfe94e2e3c57643ee3fcd04171eff",
+    "url": "/Covid-Tracker-Live/index.html"
+  },
+  {
+    "revision": "443df119f9c730375686",
+    "url": "/Covid-Tracker-Live/static/css/main.256573bf.chunk.css"
+  },
+  {
+    "revision": "47152f689fbe92589c43",
+    "url": "/Covid-Tracker-Live/static/js/2.f5f28611.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker-Live/static/js/2.f5f28611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443df119f9c730375686",
+    "url": "/Covid-Tracker-Live/static/js/main.9a307db3.chunk.js"
+  },
+  {
+    "revision": "1ea073a74f44e9ac2f1e",
+    "url": "/Covid-Tracker-Live/static/js/runtime-main.f57e6a42.js"
+  },
+  {
+    "revision": "cd71d8efb175b2071db765eca71dac85",
+    "url": "/Covid-Tracker-Live/static/media/COVID.cd71d8ef.jpg"
+  }
+]);
